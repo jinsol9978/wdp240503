f6746e1ecf139516b28327c10afbd7507c1e3a63
jinsol9978 <jinsol9978@gmail.com>
2024-06-11 14:33:59 +0900

    item

--- 04-css/js/comm.js
+++ 04-css/js/comm.js
@@ -72,5 +72,17 @@ $(function () {
                 }
             })
         })
+        // aos
+        AOS.init({
+            // key:value,
+            offset: 100,
+            delay: 0,
+            duration: 600,
+            easing: 'ease-in-out',
+            once: false,
+            mirror: false,
+
+            anchorPlacement: 'top-center',
+        })
     })
 })
